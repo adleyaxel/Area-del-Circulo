8901303880ffd7273cd2caa5163dde2f2d8b4d65
adleyaxel <adleymarchena@gmail.com>
2022-06-14 22:34:17 -0600

    Area-C

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Variables
+double r, a;
+const double PI = 3.1416;
+
+//Entrada 
+System.Console.Write("Valor del radio: ");
+r = double.Parse(System.Console.ReadLine());
+
+//Proceso
+a =PI * Math.Pow(r, 2);
+
+//Salida
+System.Console.Write("El area es de : " + a);
+System.Console.ReadKey();
